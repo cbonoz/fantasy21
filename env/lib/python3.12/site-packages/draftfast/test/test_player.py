6cde2e135179bb9ca3a0e52203280fe3292eea8b
Chris Buonocore <chrisdistrict@gmail.com>
2024-08-18 15:10:45 -0400

    fix ignore

--- env/lib/python3.12/site-packages/draftfast/test/test_player.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from draftfast.orm import Player
-
-assertions = unittest.TestCase("__init__")
-
-
-def test_player_value():
-    pg = Player(name="A", cost=5500, proj=55, pos="PG")
-    assertions.assertEqual(pg.value, 10)
